c7f66be5cdc53517c3f3a49ecd926771f76c037d
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-15 15:44:54 -0600

    fit: added map method to render tracks

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Track from "../Track/Track";
 import "./TrackList.css";
 
 export class TrackList extends React.Component {
 	render() {
 		return (
 			<div className="TrackList">
-				{/* will add a map method that renders a set of Track components */}
+				{/* a map method that renders a set of Track components */}
+				{this.props.tracks.map((track) => {
+					return <Track key={track.id} />;
+				})}
 			</div>
 		);
 	}
