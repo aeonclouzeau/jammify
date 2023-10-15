9fa082673330067d56a47627f1c5faee86264a9c
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-15 14:53:50 -0600

    fix: changed class attribute to className

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,7 +3,7 @@ import React from "react";
 export class Playlist extends React.Component {
 	render() {
 		return (
-			<div class="Playlist">
+			<div className="Playlist">
 				<input value="New Playlist" />
 				{/* add tracklist component */}
 				<button class="Playlist-save">SAVE TO SPOTIFY</button>
