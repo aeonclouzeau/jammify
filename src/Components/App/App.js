8502d45609d0a75830850d333c4ed04daa86329c
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-15 14:48:48 -0600

    fit: imported and added Playlist component to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { SearchBar } from "../SearchBar/SearchBar";
 import { SearchResults } from "../SearchResults/SearchResults";
+import { Playlist } from "../Playlist/Playlist";
 
 class App extends React.Component {
 	render() {
@@ -17,6 +18,7 @@ class App extends React.Component {
 						{/* SearchbarResults component */}
 						<SearchResults />
 						{/* Playlist component */}
+						<Playlist />
 					</div>
 				</div>
 			</div>
