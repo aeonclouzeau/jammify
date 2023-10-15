68b3b67b13e7cabb92718486d800c1ec218837e4
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-15 15:04:54 -0600

    fix, fit: Added TrackList files, changed className

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Playlist.css";
 
 export class Playlist extends React.Component {
 	render() {
 		return (
 			<div className="Playlist">
-				<input value="New Playlist" />
+				<input defaultValue={"New Playlist"} />
 				{/* add tracklist component */}
 				<button class="Playlist-save">SAVE TO SPOTIFY</button>
 			</div>
