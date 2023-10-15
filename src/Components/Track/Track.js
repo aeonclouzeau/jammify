6ba0d6d9bc265f86a3a1eadbe28b2861dab0945f
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-15 15:18:19 -0600

    fit: added boilerplate for Track react

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class Track extends React.Component {
+	render() {
+		return (
+			<div className="Track">
+				<div className="Track-information">
+					{/* <h3>Track Name</h3> */}
+					{/* <p>track artist | track album</p> */}
+				</div>
+				{/* <button class="Track-action">+ or -</button> */}
+			</div>
+		);
+	}
+}
