6f221232a7c6f8cfcc9ca8bbc120d95c4d6d783a
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-16 08:57:44 -0600

    passed onAdd props to Track from TrackList

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -8,7 +8,9 @@ export class TrackList extends React.Component {
 			<div className="TrackList">
 				{/* a map method that renders a set of Track components */}
 				{this.props.tracks.map((track) => {
-					return <Track key={track.id} track={track} />;
+					return (
+						<Track key={track.id} track={track} onAdd={this.props.onAdd} />
+					);
 				})}
 			</div>
 		);
