e6c8195be43214c1791ee5e30d15fe611b037580
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-16 08:54:38 -0600

    passed isRemoval with false val to traclist comp

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -8,7 +8,11 @@ export class SearchResults extends React.Component {
 			<div className="SearchResults">
 				<h2>Results</h2>
 				{/* tracklist component */}
-				<TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} />
+				<TrackList
+					tracks={this.props.searchResults}
+					onAdd={this.props.onAdd}
+					isRemoval={false}
+				/>
 			</div>
 		);
 	}
