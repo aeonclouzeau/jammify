eaeeed735ff7c2b47fd58d1b797bc537cbf98150
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-17 19:53:32 -0600

    binded saveplaylist method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -42,6 +42,7 @@ class App extends React.Component {
 		this.addTrack = this.addTrack.bind(this);
 		this.removeTrack = this.removeTrack.bind(this);
 		this.updatePlaylistName = this.updatePlaylistName.bind(this);
+		this.savePlaylist = this.savePlaylist.bind(this);
 	}
 
 	addTrack(track) {
