a12e6207b4e32f8e010826f1e6627c9c00803294
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-17 19:41:54 -0600

    fit: constructor and super props to bind method

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,6 +3,11 @@ import "./Playlist.css";
 import { TrackList } from "../TrackList/TrackList";
 
 export class Playlist extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleNameChange = this.handleNameChange.bind(this);
+	}
+
 	handleNameChange(e) {
 		this.props.onNameChange(e.target.value);
 	}
