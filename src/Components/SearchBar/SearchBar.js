aee0059f04566b99257061e427d11bad2e100a23
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-17 20:12:06 -0600

    fit: added handler for change term

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,16 +5,24 @@ export class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
 		this.search = this.search.bind(this);
+		this.handleTermChange = this.handleTermChange.bind(this);
 	}
 
 	search() {
 		this.props.onSearch(this.state.term);
 	}
 
+	handleTermChange(e) {
+		this.setState({ term: e.target.value });
+	}
+
 	render() {
 		return (
 			<div className="SearchBar">
-				<input placeholder="Enter a song, album or artist" />
+				<input
+					placeholder="Enter a song, album or artist"
+					onChange={this.handleTermChange}
+				/>
 				<button className="SearchButton">SEARCH</button>
 			</div>
 		);
