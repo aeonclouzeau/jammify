3fbdedf40f3df04c78e3256cad2c9617ace45e06
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-18 19:35:39 -0600

    fit: implemented the searchresults

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,7 @@ import { SearchBar } from "../SearchBar/SearchBar";
 import { SearchResults } from "../SearchResults/SearchResults";
 import { Playlist } from "../Playlist/Playlist";
 import "./App.css";
+import { Spotify } from "../../util/Spotify";
 
 class App extends React.Component {
 	constructor(props) {
@@ -75,7 +76,10 @@ class App extends React.Component {
 	}
 
 	search(term) {
-		console.log(term);
+		Spotify.search(term).then((result) => {
+			this.setState({ searchResults: result });
+		});
+		// console.log(term);
 	}
 
 	render() {
