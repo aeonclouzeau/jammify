a5fac7c4b2d82c1d2dfaf19965a8da64e2106050
Aeon Clouzeau <aeonruizc@gmail.com>
2023-10-18 18:24:27 -0600

    set the logic to obtain acces token and expiration

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,12 +1,19 @@
-const accessToken = "";
+let accessToken = "";
+const clientID = "f35f548e116b43579f4afb64ecb18afc";
+const redirectURI = "http://localhost:3000/";
 const Spotify = {
 	getAccessToken() {
 		if (accessToken) {
 			return accessToken;
 		}
 		// Implicit Grant Flow
-		window.location.href.match(/access_token=([^&]*)/); // gets the access token from the request
-		window.location.href.match(/expires_in=([^&]*)/); // gets the expiration time for the token
+		const urlAccessToken = window.location.href.match(/access_token=([^&]*)/); // gets the access token from the request
+		const urlExpiresIn = window.location.href.match(/expires_in=([^&]*)/); // gets the expiration time for the token
+
+		if (urlAccessToken && urlExpiresIn) {
+			accessToken = urlAccessToken[1];
+			const expires_in = Number(urlExpiresIn[1]);
+		}
 	},
 };
 
